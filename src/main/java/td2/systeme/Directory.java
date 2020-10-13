b7dff5694fc2d81f25732fdc1aa00c4181057ad7
anisoaravida <Olaf120697>
2020-10-13 17:32:25 +0200

    1

--- src/main/java/td2/systeme/Directory.java
+++ src/main/java/td2/systeme/Directory.java
@@ -1,4 +1,12 @@
 package td2.systeme;
 
 public class Directory {
+    private String name;
+    private ComposantType type;
+
+    public Directory(){
+        this.name=name;
+        this.type= type;
+    }
+
 }
